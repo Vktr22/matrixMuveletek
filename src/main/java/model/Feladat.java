08d86169a44b9276e063671ac0d74423ce5b25b1
Vktr22 <nds.viktoria@gmail.com>
2026-01-11 20:58:03 +0100

    kesz

--- src/main/java/model/Feladat.java
+++ src/main/java/model/Feladat.java
@@ -1,8 +1,65 @@
 package model;
 
+import java.util.Random;
+
 public class Feladat {
+    private int sor;
+    private int oszlop;
+    private int[][] matrixA;
+    private int[][] matrixB;
 
+    public Feladat(int sor, int oszlop){
+        this.sor = sor;
+        this.oszlop=oszlop;
+        matrixA = new int[sor][oszlop];
+        matrixB = new int[sor][oszlop];
+        feltolt();
+    }
 
+    public int[][] getMatrixB() {
+        return matrixB;
+    }
+
+    public int[][] getMatrixA() {
+        return matrixA;
+    }
+
+    private void feltolt(){
+        feltoltMatrix(matrixA);
+        feltoltMatrix(matrixB);
+    }
+
+    private void feltoltMatrix(int[][] matrix) {
+        Random random = new Random();
+        for (int i = 0; i < sor; i++) {
+            for (int j = 0; j < oszlop; j++) {
+                matrix[i][j] = random.nextInt(26)-10;
+            }
+        }
+    }
+
+    public int[][] osszead(){
+        int [][] eredmeny = new int[sor][oszlop];
+        for (int i = 0; i < sor; i++) {
+            for (int j = 0; j < oszlop; j++) {
+                int osszeg = matrixA[i][j] + matrixB[i][j];
+                if(osszeg==0){
+                    osszeg=1;
+                }
+                eredmeny[i][j] = osszeg;
+            }
+        }
+        return  eredmeny;
+    }
+
+    public void kiir(int [][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.printf("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
 
 }
---
+++ src/test/java/teszt/matrixTeszt.java
@@ -0,0 +1,49 @@
+package teszt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.Feladat;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class matrixTeszt {
+    int sor = 5;
+    int oszlop = 6;
+    Feladat feladat = new Feladat(sor, oszlop);
+    List<int[][]> matrixok = List.of(
+            feladat.getMatrixA(),
+            feladat.getMatrixB()
+    );
+
+
+    @Test
+    void matrixMeretMegfeleloTest(){
+        try {
+            for (int[][] matrix : matrixok) {
+                assert matrix.length == sor;
+                assert matrix[0].length == oszlop;
+            }
+
+        } catch (IllegalArgumentException e) {
+            assert false : "A mátrix mérete nem megfelelő";
+        }
+    }
+
+    @Test
+    void MatrixNemTartalmazhatNullatTest() {
+        try {
+
+            for (int[][] matrix : matrixok) {
+                for (int i = 0; i < matrix.length; i++) {
+                    for (int j = 0; j < matrix[i].length; j++) {
+                        assert matrix[i][j] != 0;
+                    }
+                }
+            }
+
+        } catch (IllegalArgumentException e) {
+            assert false : "A mátrix null értéket tartalmaz";
+        }
+    }
+
+}
